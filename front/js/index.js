e7b0010e8f71b1aaf6585f0b359db5140230b9da
Sarah Liberman <sarahtoukour@gmail.com>
2022-10-26 17:58:27 +0200

    affichage produits accueil

--- front/js/index.js
+++ front/js/index.js
@@ -2,31 +2,38 @@ var baliseItems = document.querySelector("#items");
 
 fetch("http://localhost:3000/api/products")
   .then((res) => res.json())
-  .then((produits) => {
-    console.log(produits);
+  .then((products) => {
+    // console.log(products);
 
-    produits.forEach(produit => {
-      console.log(produit);
+    products.forEach((product) => {
+      // console.log(product);
 
+      // baliseItems.innerHTML += `<a href="./product.html?id=${product._id}">
+      //       <article>
+      //         <img src="${product.imageUrl}" alt="${product.altTxt}">
+      //         <h3 class="productName">${product.name} </h3>
+      //         <p class="productDescription">${product.description}</p>
+      //       </article>
+      //     </a>`;
 
+      let baliseA = document.createElement("a");
+      baliseA.setAttribute("href", `./products.html?id=${product._id}`);
+      baliseItems.appendChild(baliseA);
 
+      let baliseArticle = document.createElement("article");
+      baliseA.appendChild(baliseArticle);
 
-      baliseItems.innerHTML += `<a href="./product.html?id=${produit._id}">
-            <article>
-              <img src="${produit.imageUrl}" alt="${produit.altTxt}">
-              <h3 class="productName">${produit.name} </h3>
-              <p class="productDescription">${produit.description}</p>
-            </article>
-          </a>`;
+      let baliseImg = document.createElement("img");
+      baliseImg.setAttribute("src", `${product.imageUrl}`);
+      baliseImg.setAttribute("alt", `${product.altTxt}`);
+      baliseArticle.appendChild(baliseImg);
 
+      let baliseH3 = document.createElement("h3");
+      baliseH3.innerText = ("productName", `${product.name}`);
+      baliseArticle.appendChild(baliseH3);
 
+      let baliseP = document.createElement("p");
+      baliseP.innerText = ("productDescription", `${product.description}`);
+      baliseArticle.appendChild(baliseP);
     });
-
-
-
-
-
-
-
-  })
- 
+  });
